a6f7a4686150b67652a3ae67f62a86045ed36d4d
kamalan2k4 <117959444+kamalan2k4@users.noreply.github.com>
2022-12-27 22:55:34 -0800

    Create problem4.c

---
+++ problem4.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int x,y;
+   
+   while(1)
+ {   printf("enter x:");
+    scanf("%d",&x);
+    if(x<50)
+    printf("%d\n",y = 2*x+100);
+    else if(x==50)
+    printf("%d\n",y = 3*x+300);
+    else
+    printf("%d\n",y = 5*x-200);}
+    return 0;
